790ee6230cf96720eca8b397568bb605927fff63
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2025-03-12 13:49:26 +0100

    Changed main.py to separate_and_sort.py

--- data_gen/separate_and_sort.py
+++ data_gen/separate_and_sort.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def separate_data():
-    data = pd.read_csv('data_renew.csv')
+def separate_data(filename: str):
+    data = pd.read_csv(filename)
     return [x for _, x in data.groupby(data['pacient_id'])]
    
    
@@ -9,11 +9,11 @@ def sort_by_date(dfs):
     return [df.sort_values(by='Date_Time_measured') for df in dfs]
 
 def main():
-    dfs = separate_data()
+    dfs = separate_data('syn_data.csv')
     dfs = sort_by_date(dfs)
 
     for df in dfs:
-        df.to_csv(f'split_data/patient_{df.iloc[0]["pacient_id"]}.csv', index=False)
+        df.to_csv(f'split_syn_data/patient_{df.iloc[0]["pacient_id"]}_syn.csv', index=False)
 
 if __name__ == '__main__':
     main()
